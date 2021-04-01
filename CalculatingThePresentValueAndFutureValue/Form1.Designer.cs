ed8572785eb5e54ea0d07d966f549f9af85b801f
Raikhana <raikhanaurazbaeva@gmail.com>
2021-04-01 11:19:40 -0600

    Add project files.

---
+++ CalculatingThePresentValueAndFutureValue/Form1.Designer.cs
@@ -0,0 +1,278 @@
+﻿namespace CalculatingThePresentValueAndFutureValue
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtPresentValue = new System.Windows.Forms.TextBox();
+            this.txtInterestRate = new System.Windows.Forms.TextBox();
+            this.txtPeriod = new System.Windows.Forms.TextBox();
+            this.txtFutureValue = new System.Windows.Forms.TextBox();
+            this.btnResetDefaults = new System.Windows.Forms.Button();
+            this.btnSent = new System.Windows.Forms.Button();
+            this.btnReload = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Palatino Linotype", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(19, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 17);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Present Value";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Palatino Linotype", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(19, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 17);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Interest Rate";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Palatino Linotype", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(19, 81);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 17);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Period";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Palatino Linotype", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(19, 111);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(80, 17);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Future Value";
+            // 
+            // txtPresentValue
+            // 
+            this.txtPresentValue.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtPresentValue.Font = new System.Drawing.Font("Palatino Linotype", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPresentValue.Location = new System.Drawing.Point(166, 18);
+            this.txtPresentValue.Name = "txtPresentValue";
+            this.txtPresentValue.Size = new System.Drawing.Size(181, 24);
+            this.txtPresentValue.TabIndex = 1;
+            this.txtPresentValue.MouseClick += new System.Windows.Forms.MouseEventHandler(this.txtPresentValue_MouseClick);
+            this.txtPresentValue.TextChanged += new System.EventHandler(this.txtPresentValue_TextChanged);
+            // 
+            // txtInterestRate
+            // 
+            this.txtInterestRate.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtInterestRate.Font = new System.Drawing.Font("Palatino Linotype", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtInterestRate.Location = new System.Drawing.Point(166, 48);
+            this.txtInterestRate.Name = "txtInterestRate";
+            this.txtInterestRate.ReadOnly = true;
+            this.txtInterestRate.Size = new System.Drawing.Size(181, 24);
+            this.txtInterestRate.TabIndex = 2;
+            this.txtInterestRate.MouseClick += new System.Windows.Forms.MouseEventHandler(this.txtInterestRate_MouseClick);
+            this.txtInterestRate.TextChanged += new System.EventHandler(this.txtInterestRate_TextChanged);
+            // 
+            // txtPeriod
+            // 
+            this.txtPeriod.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtPeriod.Font = new System.Drawing.Font("Palatino Linotype", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPeriod.Location = new System.Drawing.Point(166, 78);
+            this.txtPeriod.Name = "txtPeriod";
+            this.txtPeriod.ReadOnly = true;
+            this.txtPeriod.Size = new System.Drawing.Size(181, 24);
+            this.txtPeriod.TabIndex = 3;
+            this.txtPeriod.MouseClick += new System.Windows.Forms.MouseEventHandler(this.txtPeriod_MouseClick);
+            this.txtPeriod.TextChanged += new System.EventHandler(this.txtPeriod_TextChanged);
+            // 
+            // txtFutureValue
+            // 
+            this.txtFutureValue.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtFutureValue.Font = new System.Drawing.Font("Palatino Linotype", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtFutureValue.Location = new System.Drawing.Point(166, 108);
+            this.txtFutureValue.Name = "txtFutureValue";
+            this.txtFutureValue.ReadOnly = true;
+            this.txtFutureValue.Size = new System.Drawing.Size(181, 24);
+            this.txtFutureValue.TabIndex = 4;
+            this.txtFutureValue.MouseClick += new System.Windows.Forms.MouseEventHandler(this.txtFutureValue_MouseClick);
+            this.txtFutureValue.TextChanged += new System.EventHandler(this.txtFutureValue_TextChanged);
+            // 
+            // btnResetDefaults
+            // 
+            this.btnResetDefaults.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnResetDefaults.Font = new System.Drawing.Font("Palatino Linotype", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnResetDefaults.Location = new System.Drawing.Point(22, 156);
+            this.btnResetDefaults.Name = "btnResetDefaults";
+            this.btnResetDefaults.Size = new System.Drawing.Size(100, 23);
+            this.btnResetDefaults.TabIndex = 5;
+            this.btnResetDefaults.Text = "Reset defaults";
+            this.btnResetDefaults.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnResetDefaults.UseVisualStyleBackColor = true;
+            this.btnResetDefaults.Click += new System.EventHandler(this.btnResetDefaults_Click);
+            // 
+            // btnSent
+            // 
+            this.btnSent.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnSent.Font = new System.Drawing.Font("Palatino Linotype", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSent.Location = new System.Drawing.Point(197, 156);
+            this.btnSent.Name = "btnSent";
+            this.btnSent.Size = new System.Drawing.Size(99, 23);
+            this.btnSent.TabIndex = 7;
+            this.btnSent.Text = "Sent this result";
+            this.btnSent.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnSent.UseVisualStyleBackColor = true;
+            this.btnSent.Click += new System.EventHandler(this.btnSent_Click);
+            // 
+            // btnReload
+            // 
+            this.btnReload.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnReload.Font = new System.Drawing.Font("Palatino Linotype", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReload.Location = new System.Drawing.Point(129, 156);
+            this.btnReload.Name = "btnReload";
+            this.btnReload.Size = new System.Drawing.Size(62, 23);
+            this.btnReload.TabIndex = 6;
+            this.btnReload.Text = "Reload";
+            this.btnReload.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnReload.UseVisualStyleBackColor = true;
+            this.btnReload.Click += new System.EventHandler(this.btnReload_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnExit.Font = new System.Drawing.Font("Palatino Linotype", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(298, 156);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(49, 23);
+            this.btnExit.TabIndex = 8;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Palatino Linotype", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(147, 18);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(14, 17);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "$";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Palatino Linotype", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(145, 48);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(18, 17);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "%";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Palatino Linotype", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(147, 108);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(14, 17);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "$";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Palatino Linotype", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(126, 78);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(40, 17);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "years";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnExit;
+            this.ClientSize = new System.Drawing.Size(371, 199);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnReload);
+            this.Controls.Add(this.btnSent);
+            this.Controls.Add(this.btnResetDefaults);
+            this.Controls.Add(this.txtFutureValue);
+            this.Controls.Add(this.txtPeriod);
+            this.Controls.Add(this.txtInterestRate);
+            this.Controls.Add(this.txtPresentValue);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtPresentValue;
+        private System.Windows.Forms.TextBox txtInterestRate;
+        private System.Windows.Forms.TextBox txtPeriod;
+        private System.Windows.Forms.TextBox txtFutureValue;
+        private System.Windows.Forms.Button btnResetDefaults;
+        private System.Windows.Forms.Button btnSent;
+        private System.Windows.Forms.Button btnReload;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ CalculatingThePresentValueAndFutureValue/Form1.cs
@@ -0,0 +1,260 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculatingThePresentValueAndFutureValue
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        double presentValue;
+        double InterestRate;
+        double period;
+        double futureValue;
+
+        string[,] calculationsTable = new string[10, 4];
+        int row = 0;
+
+        string[,] calculationsTable1 = new string[10, 4];
+        int row1 = 0;
+
+        private void CalculatingTheFutureValue()
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(txtPresentValue.Text))
+                {
+                    presentValue = 0;
+                }
+                else
+                {
+                    presentValue = double.Parse(txtPresentValue.Text);
+                }
+                InterestRate = double.Parse(txtInterestRate.Text);
+                period = double.Parse(txtPeriod.Text);
+
+                double p = InterestRate / 100;
+                double exponentiation = Math.Pow((1 + p), period);
+                futureValue = Math.Round(presentValue * exponentiation);
+
+                txtFutureValue.Text = futureValue.ToString();
+
+                calculationsTable[row, 0] = presentValue.ToString("c");
+                calculationsTable[row, 1] = InterestRate.ToString();
+                calculationsTable[row, 2] = period.ToString();
+                calculationsTable[row, 3] = futureValue.ToString("C");
+                //row++;
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Invalid numeric format. Please check all entries.", "Entry Error");
+            }
+            catch (OverflowException)
+            {
+                MessageBox.Show("Overflow error. Please enter smaller values.", "Entry Error");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, ex.GetType().ToString());
+            }
+        }
+
+        private void txtPresentValue_TextChanged(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtInterestRate.Text) ||
+                string.IsNullOrEmpty(txtPeriod.Text))
+            {}
+            else {
+                if (txtPresentValue.ReadOnly == false)
+                {
+                    CalculatingTheFutureValue();
+                }
+            }
+        }
+
+        private void txtInterestRate_TextChanged(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtInterestRate.Text) ||
+                string.IsNullOrEmpty(txtPresentValue.Text) ||
+                string.IsNullOrEmpty(txtPeriod.Text))
+            {}
+            else {
+                if (txtInterestRate.ReadOnly == false)
+                {
+                    CalculatingTheFutureValue();
+                }
+            }
+        }
+
+        private void txtPeriod_TextChanged(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtPresentValue.Text) ||
+                string.IsNullOrEmpty(txtInterestRate.Text) ||
+                string.IsNullOrEmpty(txtPeriod.Text))
+            {}
+            else {
+                if (txtPeriod.ReadOnly == false)
+                {
+                    CalculatingTheFutureValue();
+                }
+            }
+        }
+
+        private void CalculatingThePresentValue()
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(txtFutureValue.Text))
+                {
+                    futureValue = 0;
+                }
+                else
+                {
+                    futureValue = double.Parse(txtFutureValue.Text);
+                }
+                InterestRate = double.Parse(txtInterestRate.Text);
+                period = double.Parse(txtPeriod.Text);
+
+                double p = InterestRate / 100;
+                double exponentiation = Math.Pow((1 + p), period);
+                presentValue = Math.Round(futureValue / exponentiation, 2);
+
+                txtPresentValue.Text = presentValue.ToString();
+
+                calculationsTable1[row, 0] = presentValue.ToString("c");
+                calculationsTable1[row, 1] = InterestRate.ToString();
+                calculationsTable1[row, 2] = period.ToString();
+                calculationsTable1[row, 3] = futureValue.ToString("C");
+                //row1++;
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Invalid numeric format. Please check all entries.", "Entry Error");
+            }
+            catch (OverflowException)
+            {
+                MessageBox.Show("Overflow error. Please enter smaller values.", "Entry Error");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, ex.GetType().ToString());
+            }
+        }
+
+        private string value;
+        private void txtFutureValue_TextChanged(object sender, EventArgs e)
+        {
+            if(value1 == true)
+            {
+                value1 = false;
+            }
+            else
+            {
+                var oldValue = value;
+                value = ((TextBox)sender).Text; // = txtFutureValue.Text
+
+                if (oldValue != null)
+                {
+                    if (txtFutureValue.ReadOnly == false)
+                    {
+                        CalculatingThePresentValue();
+                    }
+                }
+            }
+        }
+
+        private void txtPresentValue_MouseClick(object sender, MouseEventArgs e)
+        {
+            txtPresentValue.ReadOnly = false;
+            txtInterestRate.ReadOnly = true;
+            txtPeriod.ReadOnly = true;
+            txtFutureValue.ReadOnly = true;
+        }
+
+        private void txtInterestRate_MouseClick(object sender, MouseEventArgs e)
+        {
+            txtPresentValue.ReadOnly = true;
+            txtInterestRate.ReadOnly = false;
+            txtPeriod.ReadOnly = true;
+            txtFutureValue.ReadOnly = true;
+        }
+
+        private void txtPeriod_MouseClick(object sender, MouseEventArgs e)
+        {
+            txtPresentValue.ReadOnly = true;
+            txtInterestRate.ReadOnly = true;
+            txtPeriod.ReadOnly = false;
+            txtFutureValue.ReadOnly = true;
+        }
+
+        private void txtFutureValue_MouseClick(object sender, MouseEventArgs e)
+        {
+            txtPresentValue.ReadOnly = true;
+            txtInterestRate.ReadOnly = true;
+            txtPeriod.ReadOnly = true;
+            txtFutureValue.ReadOnly = false;
+        }
+
+        bool value1 = false;
+        private void btnResetDefaults_Click(object sender, EventArgs e)
+        {
+            value1 = true;
+            txtPresentValue.Clear();
+            txtInterestRate.Clear();
+            txtPeriod.Clear();
+            txtFutureValue.Clear();
+        }
+
+        private void btnReload_Click(object sender, EventArgs e)
+        {
+            txtPresentValue.Clear();
+            txtPeriod.Clear();
+            txtFutureValue.Clear();
+        }
+
+        private void btnSent_Click(object sender, EventArgs e)
+        {
+            string message = "Pr/Value  \tRate   \tPeriod    \tFt/Value\n";
+            for (int i = 0; i < calculationsTable.GetLength(0); i++)
+            {
+                if (calculationsTable[i, 0] != null)
+                {
+                    for (int j = 0; j < calculationsTable.GetLength(1); j++)
+                    {
+                        message += calculationsTable[i, j] + "\t";
+                    }
+                    message += "\n";
+                }
+            }
+            MessageBox.Show(message, "Future Value Calculations");
+
+            string message1 = "Pr/Value  \tRate   \tPeriod    \tFt/Value\n";
+            for (int i = 0; i < calculationsTable1.GetLength(0); i++)
+            {
+                if (calculationsTable1[i, 0] != null)
+                {
+                    for (int j = 0; j < calculationsTable1.GetLength(1); j++)
+                    {
+                        message1 += calculationsTable1[i, j] + "\t";
+                    }
+                    message1 += "\n";
+                }
+            }
+            MessageBox.Show(message1, "Present Value Calculations");
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
